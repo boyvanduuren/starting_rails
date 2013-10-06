fa02569010155c36fc14ec6d8a0d8230e85e9fa0
bcd <bcvanduuren@gmail.com>
2013-10-06 12:05:49 +0200

    Forgot to change authentication for controller comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+	http_basic_authenticate_with name: Settings.admin_login,
+		password: Settings.admin_password, except: [:index, :show]
 
 	def create
 		@post = Post.find(params[:post_id])
